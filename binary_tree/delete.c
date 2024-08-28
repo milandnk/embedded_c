9037bbc6f8a42f1f9341ed95fffc95207b9ed800
himanshu69 <himanshu69>
2024-08-28 18:02:03 +0530

    28/08

---
+++ binary_tree/delete.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *create_node(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void in_order(struct node *root)
+{
+    if (root != NULL)
+    {
+        in_order(root->left);
+        printf("%d  ", root->data);
+        in_order(root->right);
+    }
+}
+
+int is_bst_util(struct node *root, struct node **prev)
+{
+    if (root != NULL)
+    {
+        if (!is_bst_util(root->left, prev))
+        {
+            return 0;
+        }
+        if (*prev != NULL && root->data <= (*prev)->data)
+        {
+            return 0;
+        }
+        *prev = root;
+        return is_bst_util(root->right, prev);
+    }
+    return 1;
+}
+
+int is_bst(struct node *root)
+{
+    struct node *prev = NULL;
+    return is_bst_util(root, &prev);
+}
+
+struct node* delete(struct node *root, int key)
+{
+    struct node *prev = NULL;
+    while (root != NULL)
+    {
+        prev = root;
+        if (root->data == key)
+        {
+            printf("insertion is not possible");
+            return;
+        }
+        else if (root->data > key)
+        {
+            root = root->left;
+        }
+        else
+        {
+            root = root->right;
+        }
+    }
+    struct node *new = create_node(key);
+    if (prev->data > key)
+    {
+        prev->left = new;
+    }
+    else
+    {
+        prev->right = new;
+    }
+}
+
+int main()
+{
+    int val;
+    struct node *p = create_node(4);
+    struct node *p1 = create_node(2);
+    struct node *p2 = create_node(1);
+    struct node *p3 = create_node(3);
+    struct node *p4 = create_node(5);
+
+    p->left = p1;
+    p->right = p4;
+    p1->left = p2;
+    p1->right = p3;
+
+    in_order(p);
+    printf("\n");
+
+    if (is_bst(p))
+    {
+        printf("This is a BST!!\n");
+    }
+    else
+    {
+        printf("This is not a BST!!\n");
+    }
+
+    printf("Enter value to insert: ");
+    scanf("%d", &val);
+    insert(p, val);
+    in_order(p);
+    printf("\n");
+
+    free(p);
+    free(p1);
+    free(p2);
+    free(p3);
+    free(p4);
+
+    return 0;
+}
---
+++ binary_tree/find_key_iter.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *create_node(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void pre_order(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d  ", root->data);
+        pre_order(root->left);
+        pre_order(root->right);
+    }
+}
+
+void post_order(struct node *root)
+{
+    if (root != NULL)
+    {
+        post_order(root->left);
+        post_order(root->right);
+        printf("%d  ", root->data);
+    }
+}
+
+void in_order(struct node *root)
+{
+    if (root != NULL)
+    {
+        in_order(root->left);
+        printf("%d  ", root->data);
+        in_order(root->right);
+    }
+}
+
+int is_bst_util(struct node *root, struct node **prev)
+{
+    if (root != NULL)
+    {
+        if (!is_bst_util(root->left, prev))
+        {
+            return 0;
+        }
+        if (*prev != NULL && root->data <= (*prev)->data)
+        {
+            return 0;
+        }
+        *prev = root;
+        return is_bst_util(root->right, prev);
+    }
+    return 1;
+}
+
+int is_bst(struct node *root)
+{
+    struct node *prev = NULL;
+    return is_bst_util(root, &prev);
+}
+
+struct node *search_iter(struct node *root, int key)
+{
+    while (root != NULL)
+    {
+        if (root->data == key)
+        {
+            return root;
+        }
+        if (root->data > key)
+        {
+            root = root->left;
+        }
+        if (root->data < key)
+        {
+            root = root->right;
+        }
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    int val;
+    struct node *p = create_node(4);
+    struct node *p1 = create_node(2);
+    struct node *p2 = create_node(1);
+    struct node *p3 = create_node(3);
+    struct node *p4 = create_node(5);
+
+    p->left = p1;
+    p->right = p4;
+    p1->left = p2;
+    p1->right = p3;
+
+    in_order(p);
+    printf("\n");
+
+    if (is_bst(p))
+    {
+        printf("This is a BST!!\n");
+    }
+    else
+    {
+        printf("This is not a BST!!\n");
+    }
+
+    printf("Enter value to find: ");
+    scanf("%d", &val);
+    struct node *n = search_iter(p, val);
+
+    if (n != NULL)
+    {
+        printf("Value %d is available!!\n", val);
+    }
+    else
+    {
+        printf("Value %d is not available!!\n", val);
+    }
+
+    free(p);
+    free(p1);
+    free(p2);
+    free(p3);
+    free(p4);
+
+    return 0;
+}
